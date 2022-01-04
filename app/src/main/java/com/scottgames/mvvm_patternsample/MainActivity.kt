1a05f3999dc1a5ed366ce14bad5ab4f6efedc33c
johnzieman <j.g.zokirov@gmail.com>
2022-01-04 12:31:32 +0500

    add navhost

--- app/src/main/java/com/scottgames/mvvm_patternsample/MainActivity.kt
+++ app/src/main/java/com/scottgames/mvvm_patternsample/MainActivity.kt
@@ -6,15 +6,8 @@ import android.widget.TextView
 import androidx.activity.viewModels
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var textView: TextView
-    private val mainViewModel: MainViewModel by viewModels { ViewModelFactory(application, "Hello") }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        textView = findViewById(R.id.textView)
-
-        mainViewModel.time.observe(this){
-            textView.text = it
-        }
     }
 }
