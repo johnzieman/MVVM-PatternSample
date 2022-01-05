c1353cddeb942f7ac1c91a3e2ca1e73d5fcbe691
johnzieman <j.g.zokirov@gmail.com>
2022-01-05 12:28:59 +0500

    MainActivity initialized

--- app/src/main/java/com/scottgames/mvvm_patternsample/MainActivity.kt
+++ app/src/main/java/com/scottgames/mvvm_patternsample/MainActivity.kt
@@ -4,10 +4,20 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
 import androidx.activity.viewModels
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import com.scottgames.mvvm_patternsample.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setSupportActionBar(binding.toolbar)
+        title = "Notes"
+
+        navController = Navigation.findNavController(this, R.id.nav_host_fragment_container)
     }
 }
