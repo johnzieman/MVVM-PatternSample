d0175eeb5bb4962bbf0a8d448ccbee9f01e39f94
johnzieman <j.g.zokirov@gmail.com>
2022-01-06 11:40:54 +0500

    database init

---
+++ app/src/main/java/com/scottgames/mvvm_patternsample/database/DataBaseRepository.kt
@@ -0,0 +1,10 @@
+package com.scottgames.mvvm_patternsample.database
+
+import androidx.lifecycle.LiveData
+import com.scottgames.mvvm_patternsample.models.AppNote
+
+interface DataBaseRepository {
+    val allNotes: LiveData<List<AppNote>>
+    suspend fun insert(note: AppNote, onSuccess: () -> Unit)
+    suspend fun delete(note: AppNote, onSuccess: () -> Unit)
+}
