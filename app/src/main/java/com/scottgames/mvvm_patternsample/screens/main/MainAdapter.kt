47c0ba2fd5792cd1b0b6427f26def2a922fc7f87
johnzieman <j.g.zokirov@gmail.com>
2022-01-07 19:12:35 +0500

    AddNewNotes observer

---
+++ app/src/main/java/com/scottgames/mvvm_patternsample/screens/main/MainAdapter.kt
@@ -0,0 +1,36 @@
+package com.scottgames.mvvm_patternsample.screens.main
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.scottgames.mvvm_patternsample.R
+import com.scottgames.mvvm_patternsample.models.AppNote
+import kotlinx.android.synthetic.main.note_item.view.*
+
+class MainAdapter: RecyclerView.Adapter<MainAdapter.MainHolder>() {
+    private var mListNotes = emptyList<AppNote>()
+
+    class MainHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val nameNote: TextView = view.item_note_name
+        val textNote: TextView = view.item_note_text
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MainHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.note_item, parent, false)
+        return MainHolder(view)
+    }
+
+    override fun getItemCount(): Int = mListNotes.size
+
+    override fun onBindViewHolder(holder: MainHolder, position: Int) {
+        holder.textNote.text = mListNotes[position].text
+        holder.nameNote.text = mListNotes[position].name
+    }
+
+    fun setList(list: List<AppNote>) {
+        mListNotes = list
+        notifyDataSetChanged()
+    }
+}
